bd4aa9d959bcf58d1854dbf74e91b89b07096bb1
chibor554-code <chibor554@gmail.com>
2026-08-20 01:14:57 +0100

    updated settings

--- portfolio_page/settings.py
+++ portfolio_page/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY =  os.getenv("SECRET_KEY")
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['my-portfolio-ikenna.onrender.com', '127.0.0.1', 'localhost']
 
