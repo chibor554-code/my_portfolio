55dd3b99f1cad012f86605acdadadaf44d28ad4f
chibor554-code <chibor554@gmail.com>
2026-09-12 16:25:14 +0100

    updated1

--- portfolio_page/settings.py
+++ portfolio_page/settings.py
@@ -57,12 +57,12 @@ TEMPLATES = [
 WSGI_APPLICATION = "portfolio.wsgi.application"
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 WSGI_APPLICATION = 'portfolio_page.wsgi.application'
 
 LANGUAGE_CODE = "en-us"
