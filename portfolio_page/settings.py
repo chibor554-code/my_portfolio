e736fe8fd301acbf79d41006f5d497d6ca4520ce
chibor554-code <chibor554@gmail.com>
2026-09-12 16:32:22 +0100

    updated

--- portfolio_page/settings.py
+++ portfolio_page/settings.py
@@ -11,7 +11,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY =  os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv('DEBUG')
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [
+    'my-portfolio-3-d6cj.onrender.com',
+    '.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 
